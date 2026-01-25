3549c736a59f43fbaa01b3380a3a1dac83fd8685
Dimitris Nimas <hello@dimitrisnimas.gr>
2026-01-25 17:07:56 +0200

    Create tailwind.config.ts

---
+++ apps/web/tailwind.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "../../packages/ui/src/**/*.{js,ts,jsx,tsx,mdx}"
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+            colors: {
+                primary: "#14b8a6", // Teal-500
+                secondary: "#3b82f6", // Blue-500
+                dark: "#0f172a", // Slate-900
+            }
+        },
+    },
+    plugins: [],
+};
+export default config;
